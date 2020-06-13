c2d87f93c5703d614c5ac1dc562fd5d6e19ef9cc
aniketg17 <aniketkg@gmail.com>
2020-06-13 20:07:12 +0530

    created new folder for screens and routes for navigation controller. Moved subject rendering to subjectScreen.js

---
+++ routes/homeStack.js
@@ -0,0 +1,9 @@
+// import {createStackNavigator} from 'react-navigation-stack';
+// import {createAppContainer} from 'react-navigation';
+// import Home from
+
+// const screens = {
+//     Home :
+// }
+
+// const HomeStack = createStackNavigator();
---
+++ screens/subjectScreen.js
@@ -0,0 +1,65 @@
+import React, {useState, useEffect} from 'react';
+import {
+  StyleSheet,
+  ListView,
+  FlatList,
+  View,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+import 'react-native-gesture-handler';
+import {pink} from 'color-name';
+import {NavigationContainer} from '@react-navigation/native';
+
+const SubjectScreen = () => {
+  const [subjects, setSubject] = useState([]);
+  const SUBJECT_QUERY_URL = 'http://api.purdue.io/odata/Subjects';
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(SUBJECT_QUERY_URL)
+      .then(response => response.json())
+      .then(json => setSubject(json.value))
+      .catch(error => console.error(error))
+      .finally(() => setLoading(false));
+  });
+
+  const getSubjectList = param => {
+    console.log(param.item.Name);
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={subjects}
+        keyExtractor={({SubjectId}) => SubjectId}
+        renderItem={param => (
+          <TouchableOpacity
+            style={styles.item}
+            onPress={() => getSubjectList(param)}>
+            <Text>
+              {param.item.Name} ({param.item.Abbreviation})
+            </Text>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 50,
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+  },
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24,
+  },
+});
+
+export default SubjectScreen;
