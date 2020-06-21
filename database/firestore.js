55ffbdda7f5a16000ccb05c4e8cca8925923742a
aniketg17 <aniketkg@gmail.com>
2020-06-21 13:03:10 +0530

    added firestore credentials

---
+++ database/firestore.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+import firestore from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAYSsqiSZ-WzmsHXcTrx1xGCsUgOQ6pq8s',
+  authDomain: 'workspace-a1d89.firebaseapp.com',
+  databaseURL: 'https://workspace-a1d89.firebaseio.com',
+  projectId: 'workspace-a1d89',
+  storageBucket: 'workspace-a1d89.appspot.com',
+  messagingSenderId: '117737367701',
+  appId: '1:117737367701:web:23b42bafa9906c0c65539c',
+  measurementId: 'G-FJCMQFPSDY',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.firestore();
+
+export default firebase;
