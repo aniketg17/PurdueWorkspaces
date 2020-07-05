6c4e37e1e5115db3b17cee2287f8b8d93b070f32
aniketg17 <aniketkg@gmail.com>
2020-07-05 12:01:10 +0530

    limited location query results to radius 10km of Purdue

--- screens/maps.js
+++ screens/maps.js
@@ -1,33 +1,74 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MapView from 'react-native-maps';
-import {StyleSheet, Dimensions, View} from 'react-native';
+import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
+import {StyleSheet, Text, Dimensions, View} from 'react-native';
 
-const map = () => {
+const KEY = 'AIzaSyBEk2cllJeg-VgSQbYMPmHHfceBtJ6Aa0o';
+
+const GooglePlacesInput = () => {
+  const locationChosen = useState({
+    latitude: '',
+    longitude: '',
+  });
   return (
-    <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
-    </View>
+    <GooglePlacesAutocomplete
+      placeholder="Search"
+      minLength={4} // minimum length of text to search
+      autoFocus={false}
+      returnKeyType={'search'} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
+      keyboardAppearance={'light'} // Can be left out for default keyboardAppearance https://facebook.github.io/react-native/docs/textinput.html#keyboardappearance
+      listViewDisplayed="auto" // true/false/undefined
+      fetchDetails={true}
+      renderDescription={row => row.description} // custom description render
+      onPress={(data, details = null) => {
+        // 'details' is provided when fetchDetails = true
+        console.log(data, details);
+        details.geometry.location;
+      }}
+      getDefaultValue={() => ''}
+      query={{
+        // available options: https://developers.google.com/places/web-service/autocomplete
+        location: '40.4237, -86.9212',
+        radius: '10000',
+        key: {KEY},
+        language: 'en', // language of the results
+        strictBounds: true,
+        components: 'country:us',
+      }}
+      styles={{
+        textInputContainer: {
+          width: '100%',
+        },
+        description: {
+          fontWeight: 'bold',
+        },
+        predefinedPlacesDescription: {
+          color: '#1faadb',
+        },
+      }}
+      nearbyPlacesAPI="GooglePlacesSearch" // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+      GoogleReverseGeocodingQuery={
+        {
+          // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
+        }
+      }
+      GooglePlacesSearchQuery={{
+        // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+        rankby: 'distance',
+      }}
+      GooglePlacesDetailsQuery={{
+        // available options for GooglePlacesDetails API : https://developers.google.com/places/web-service/details
+        fields: 'formatted_address',
+      }}
+      filterReverseGeocodingByTypes={[
+        // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+        // available options : https://developers.google.com/maps/documentation/geocoding/intro#Types
+        'locality',
+        'administrative_area_level_3',
+      ]}
+      debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
+    />
   );
 };
 
-let {height, width} = Dimensions.get('window');
-const styles = StyleSheet.create({
-  container: {
-    height: height,
-    width: width,
-  },
-
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-});
-
-export default map;
+export default GooglePlacesInput;
