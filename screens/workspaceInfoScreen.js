faafea3288929c96f3d97e61cd9e7ba87aad3be8
aniketg17 <aniketkg@gmail.com>
2020-08-13 20:51:33 +0530

    changed layout to include card to show space info

--- screens/workspaceInfoScreen.js
+++ screens/workspaceInfoScreen.js
@@ -1,13 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {
+  Card,
+  CardItem,
+  Text,
+  Body,
   Container,
   Content,
-  Text,
-  Header,
-  Badge,
   Icon,
-  Button,
 } from 'native-base';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 
@@ -15,13 +15,40 @@ import {TouchableOpacity} from 'react-native-gesture-handler';
 
 const InfoScreen = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.item1}>
-        <Text style={styles.text}>Item 1</Text>
-      </View>
-      <View style={styles.minicontainer}>
+    <View style={styles.mainContainer}>
+      <Container style={styles.upperContainer}>
+        <Content padder>
+          <Card style={styles.card}>
+            <CardItem header bordered>
+              <Text>NativeBase</Text>
+            </CardItem>
+            <CardItem bordered>
+              <Body>
+                <Text>
+                  NativeBase is a free and open source framework that enable
+                  developers to build high-quality mobile apps using React
+                  Native iOS and Android apps with a fusion of ES6. NativeBase
+                  is a free and open source framework that enable developers to
+                  build high-quality mobile apps using React Native iOS and
+                  Android apps with a fusion of ES6. NativeBase is a free and
+                  open source framework that enable developers to build
+                  high-quality mobile apps using React Native iOS and Android
+                  apps with a fusion of ES6. NativeBase is a free and open
+                  source framework that enable developers to build high-quality
+                  mobile apps using React Native iOS and Android apps with a
+                  fusion of ES6.
+                </Text>
+              </Body>
+            </CardItem>
+            <CardItem footer bordered>
+              <Text>GeekyAnts</Text>
+            </CardItem>
+          </Card>
+        </Content>
+      </Container>
+      <View style={styles.miniContainer}>
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.Button}>
+          <TouchableOpacity style={styles.button}>
             <Icon type="FontAwesome" name="check-circle" />
           </TouchableOpacity>
         </View>
@@ -31,22 +58,25 @@ const InfoScreen = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
     backgroundColor: '#fff',
   },
-  minicontainer: {
+  miniContainer: {
     flex: 1,
     flexDirection: 'row',
     backgroundColor: '#f0f8ff',
+  },
+  card: {
+    width: 300,
   },
   text: {
     fontWeight: 'bold',
     fontSize: 20,
   },
-  item1: {
-    flex: 3,
-    backgroundColor: '#feffcb', // #f0f8ff
+  upperContainer: {
+    flex: 2,
+    backgroundColor: '#f0f8ff', // #feffcb
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -55,7 +85,7 @@ const styles = StyleSheet.create({
     alignContent: 'center',
     paddingHorizontal: 160,
   },
-  Button: {
+  button: {
     backgroundColor: '#1e90ff',
     height: 100,
     justifyContent: 'center',
